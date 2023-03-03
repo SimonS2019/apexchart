8370345081b3a3678295bba55c1c472be0f15e95
MotivDesktop1 <shentiexiong@gmial.com>
2023-03-03 15:05:42 +1300

    mixed chaart demo done

--- apexchart/src/app/line-area/line-area.component.ts
+++ apexchart/src/app/line-area/line-area.component.ts
@@ -1,15 +1,127 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexFill,
+  ApexStroke,
+  ApexYAxis,
+  ApexTooltip,
+  ApexMarkers,
+  ApexXAxis
+} from "ng-apexcharts";
 
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  yaxis: ApexYAxis | ApexYAxis[];
+  labels: string[];
+  stroke: ApexStroke;
+  markers: ApexMarkers;
+  fill: ApexFill;
+  tooltip: ApexTooltip;
+};
 @Component({
   selector: 'app-line-area',
   templateUrl: './line-area.component.html',
   styleUrls: ['./line-area.component.css']
 })
-export class LineAreaComponent implements OnInit {
+export class LineAreaComponent {
+  @ViewChild('chart', { static: false }) chart: any;
 
-  constructor() { }
 
-  ngOnInit(): void {
+  public chartOptions: Partial<ChartOptions>| any;
+
+
+  constructor() {
+    this.chartOptions = {
+      series: [
+        {
+          name: "TEAM A",
+          type: "area",
+          data: [44, 55, 31, 47, 31, 43, 26, 41, 31, 47, 33]
+        },
+        {
+          name: "TEAM B",
+          type: "line",
+          data: [55, 69, 45, 61, 43, 54, 37, 52, 44, 61, 43]
+        }
+      ],
+      chart: {
+        height: 350,
+        type: "line"
+      },
+      stroke: {
+        curve: "smooth"
+      },
+      fill: {
+        type: "solid",
+        opacity: [0.35, 1]
+      },
+      labels: [
+        "Dec 01",
+        "Dec 02",
+        "Dec 03",
+        "Dec 04",
+        "Dec 05",
+        "Dec 06",
+        "Dec 07",
+        "Dec 08",
+        "Dec 09 ",
+        "Dec 10",
+        "Dec 11"
+      ],
+      markers: {
+        size: 0
+      },
+      yaxis: [
+        {
+          title: {
+            text: "Series A"
+          }
+        },
+        {
+          opposite: true,
+          title: {
+            text: "Series B"
+          }
+        }
+      ],
+      xaxis: {
+        labels: {
+          trim: false
+        }
+      },
+      tooltip: {
+        shared: true,
+        intersect: false,
+        y: {
+          formatter: function(y:any) {
+            if (typeof y !== "undefined") {
+              return y.toFixed(0) + " points";
+            }
+            return y;
+          }
+        }
+      }
+    };
   }
 
+  public generateData(count:any, yrange:any) {
+    var i = 0;
+    var series = [];
+    while (i < count) {
+      var x = "w" + (i + 1).toString();
+      var y =
+        Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
+
+      series.push({
+        x: x,
+        y: y
+      });
+      i++;
+    }
+    return series;
+  }
 }
