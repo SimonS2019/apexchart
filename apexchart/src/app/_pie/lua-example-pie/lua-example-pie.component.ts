8464b528c71fe28c238f09e68db8dcf51ec06a0f
MotivDesktop1 <shentiexiong@gmial.com>
2023-07-26 16:39:59 +1200

    Lua example chart done

--- apexchart/src/app/_pie/lua-example-pie/lua-example-pie.component.ts
+++ apexchart/src/app/_pie/lua-example-pie/lua-example-pie.component.ts
@@ -66,166 +66,67 @@ export class LuaExamplePieComponent  {
       series: [44, 55, 13, 43, 22],
       chart: {
         animations: {
-          enabled: true, // Front-end, default is true
-          speed: 800, // Lua, default is 800
+          enabled: true, 
         },
-        foreColor: '#373d3f', // Lua ! important "foreColor" will be overridden if we assign a color to a specific property
         sparkline: {
-          // Front-end, important!
           enabled: true,
         },
-        
         toolbar: {
           show: true, // Front-end, important!
-          tools: {
-            download: true, //Front-end
-          },
-          export: {
-            csv: {
-              // LUA, Important! , logic is in LUA
-              filename: undefined,
-              columnDelimiter: ',',
-              headerCategory: 'category',
-              headerValue: 'value',
-            },
-            svg: {
-              // LUA, Important! , logic is in lua
-              filename: 'Test 13223',
-            },
-            png: {
-              // LUA, Important! , logic is in lua
-              filename: undefined,
-            },
-          },
         },
-        type: 'donut', //Front-end,  lua to do the logical
+        type: 'donut', //Front-end,  
       },
       title: {
-        text: 'This is a big Title', //Lua, NB: using an empty string will take up some space (5px height?), we can use undefined
+        text: 'This is a big Title', //Lua
         align: 'center', //Front-end, default is 'left', Possible Options: 'left', 'center', 'right'
         style: {
           fontSize: '18px', //LUA,
-          fontWeight: 'bold', // LUA, String | Number
-          color: '#263238', //Lua
         },
       },
       subtitle: {
-        text: 'This is a subtitle', //Lua, NB: using an empty string will take up some space, we can use undefined
+        text: 'This is a subtitle', //Lua, 
         align: 'center', //Front-end, default is 'left', Possible Options: 'left', 'center', 'right'
         style: {
-          fontSize: '12px', //LUA,
-          fontWeight: 'normal', // LUA
-          color: '#9699a2', //Lua
+          fontSize: '12px', //LUA, 
         },
       },
       theme: {
-        mode: 'light', // LUA ? but automatic!  light || dark
-        // // May confuse theme.mode and chart.background.
-
+        mode: 'light', // Front end dto decide
         palette: 'palette10', //Front-end, default is "palette1"
-        // palette1 to palette10  https://apexcharts.com/docs/options/theme/#
-        // // May confuse theme.palette, colors, fill.color
       },
       labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'], // LUA? but use lua service to do this!
-      //In Axis Charts (line / column), labels can be set instead of setting xaxis categories option. While, in pie/donut charts, each label corresponds to value in series array.
       responsive: [],
       legend: {
         show: true, //Front-end, default is true
         position: 'right', ////Front-end, default is right, top, right , bottom, left
-        fontSize: '14px', //LUA, default is 12px
-        width: undefined, //LUA, undefined is means auto?
-        height: undefined, //LUA , undefined is means auto?
-        labels: {
-          colors: 'red', //LUA,
-        },
       },
       dataLabels: {
         enabled: true, //Front-end, default is true
-        style: {
-          fontSize: '14px', //LUA, default is 12px
-          fontWeight: 'bold', //LUA
-          colors: undefined, //LUA for pie and dunut chart, because we use 'foreColor'
-        },
-        background: {
-          enabled: true, //LUA, default is false
-          // foreColor: '#fff', //LUA
-          padding: 4, //LUA
-          borderRadius: 2, //LUA
-          borderWidth: 3, //LUA
-          borderColor: 'gray', //LUA
-          opacity: 0.95, //LUA
-        },
       },
       tooltip: {
         enabled: true, //Front-end, default is true
-        fillSeriesColor: false, //Front-end, default is true , When enabled, fill the tooltip background with the corresponding series color
-        theme: 'dark', //LUA, default is dark.
-        // Might be a good idea to go against the Apex theme?? Accepts either light or dark
-        style: {
-          fontSize: '12px', //LUA, default is 12px
-        },
-        marker: {
-          show: true, //Front-end, default is true
-        },
       },
-      // colors:[undefined], //LUA
       colors: ['#A300D6', '#66DA26', '#546E7A', '#E91E63', '#FF9800'], //LUA
-      // // Need? but use lua service to do this! overwrite the theme.palette
-      // // May confuse theme.palette, colors, fill.color
-      stroke: {
-        show: true, //Front-end, default is true
-        // width: 2,    //No need, Pie/ Donut need  //LUA
-      },
+      stroke: {},
       states: {},
       grid: {},
       fill: {
         type: 'gradient', // Front-end, default is solid 'solid', 'gradient' is good for pie/ donut .'pattern','image' is too fancy
-        // type: ['solid', 'gradient'],
       },
       plotOptions: {
         pie: {
           startAngle: 0, // Front-end, default is 0
           endAngle: 90, // Front-end, default is 360F
-          // startAngle: 0, // Front-end, default is 0
-          // endAngle: 360, // Front-end, default is 360
-          expandOnClick: true, // // Front-end, true
-          offsetY: 0, // LUA
-          customScale: 1, // LUA
-          dataLabels: {
-            offset: 0, // LUA, default is 0
-            minAngleToShowLabel: 10, // LUA,
-          },
           donut: {
-            // if chart.type is 'pie', the follow is not matter
             size: '65%', // Front-end, important!
-            // background: 'transparent', // No need, alwary use 'transparent'
             labels: {
               show: true, // Front-end, default is false
-              name: {
-                show: true, //  Front-end , default is true
-                fontSize: '22px', //  LUA
-                // fontWeight: 600,// // LUA, use default value is good
-                color: undefined, // LUA!
-                // offsetY: -10, // LUA?
-              },
-              value: {
-                show: true, //  Front-end , default is true
-                fontSize: '16px', //  LUA
-                // fontFamily: 'Helvetica, Arial, sans-serif',  // No need
-                // fontWeight: 400, // // No need, use default value is good
-                color: undefined, // LUA!
-                // offsetY: 16,// Might need?
-                // formatter: function (val) {
-                //   return val
-                // }
-              },
               total: {
-                show: true, //Front-end. default is false
-                showAlways: false, //LUA. default is false
+                show: true, // Lua 
                 label: 'Total', //LUA
                 fontSize: '22px', //LUA
                 fontWeight: 600, // LUA
-                color: '#373d3f', // LUA
+                color: 'black', // theme mode light = black/ #fff 
               },
             },
           },
