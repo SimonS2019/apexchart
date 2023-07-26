4902dcfd503b117de1a7c705439056d7cb137f01
MotivDesktop1 <shentiexiong@gmial.com>
2023-07-27 10:59:24 +1200

    basic timeline done

--- apexchart/src/app/_timeline/basic/basic.component.ts
+++ apexchart/src/app/_timeline/basic/basic.component.ts
@@ -1,15 +1,86 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexNonAxisChartSeries,
+  ApexChart,
+  ApexPlotOptions,
+  ApexXAxis,
+  ApexFill,
+  ApexLegend,
+  ApexResponsive,
+} from 'ng-apexcharts';
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  plotOptions: ApexPlotOptions;
+};
 
 @Component({
   selector: 'app-basic',
   templateUrl: './basic.component.html',
   styleUrls: ['./basic.component.css']
 })
-export class BasicComponent implements OnInit {
+export class BasicComponent {
+  @ViewChild('chart', { static: false }) chart: any;
+  public chartOptions: ChartOptions;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.chartOptions = {
+      series: [
+        {
+          data: [
+            {
+              x: "Code",
+              y: [
+                new Date("2019-03-02").getTime(),
+                new Date("2019-03-04").getTime()
+              ]
+            },
+            {
+              x: "Test",
+              y: [
+                new Date("2019-03-04").getTime(),
+                new Date("2019-03-08").getTime()
+              ]
+            },
+            {
+              x: "Validation",
+              y: [
+                new Date("2019-03-08").getTime(),
+                new Date("2019-03-12").getTime()
+              ]
+            },
+            {
+              x: "Deployment",
+              y: [
+                new Date("2019-03-12").getTime(),
+                new Date("2019-03-18").getTime()
+              ]
+            }
+          ]
+        }
+      ],
+      chart: {
+        height: 350,
+        type: "rangeBar"
+      },
+      plotOptions: {
+        bar: {
+          horizontal: true
+        }
+      },
+      xaxis: {
+        type: "datetime"
+      }
+    };
+    // setTimeout(() => {
+    //   console.log(JSON.stringify(this.chartOptions));
+  
+    // }, 500);
   }
 
 }
