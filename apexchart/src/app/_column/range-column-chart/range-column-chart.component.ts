a51db5a46376bda87e6994f7b143c27c1c3ed9aa
MotivDesktop1 <shentiexiong@gmial.com>
2023-08-10 11:57:27 +1200

    range-column-chart works!

--- apexchart/src/app/_column/range-column-chart/range-column-chart.component.ts
+++ apexchart/src/app/_column/range-column-chart/range-column-chart.component.ts
@@ -1,15 +1,88 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, ViewChild } from "@angular/core";
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ChartComponent,
+  ApexDataLabels,
+  ApexPlotOptions
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  dataLabels: ApexDataLabels;
+  plotOptions: ApexPlotOptions;
+};
 
 @Component({
   selector: 'app-range-column-chart',
   templateUrl: './range-column-chart.component.html',
   styleUrls: ['./range-column-chart.component.css']
 })
-export class RangeColumnChartComponent implements OnInit {
+export class RangeColumnChartComponent {
+  @ViewChild('chart', { static: false }) chart: any;
+  public chartOptions: ChartOptions;
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor() {
+    this.chartOptions = {
+      series: [
+        {
+          name: "blue",
+          data: [
+            {
+              x: "Team A",
+              y: [1, 5]
+            },
+            {
+              x: "Team B",
+              y: [4, 6]
+            },
+            {
+              x: "Team C",
+              y: [5, 8]
+            },
+            {
+              x: "Team D",
+              y: [3, 11]
+            }
+          ]
+        },
+        {
+          name: "green",
+          data: [
+            {
+              x: "Team A",
+              y: [2, 6]
+            },
+            {
+              x: "Team B",
+              y: [1, 3]
+            },
+            {
+              x: "Team C",
+              y: [7, 8]
+            },
+            {
+              x: "Team D",
+              y: [5, 9]
+            }
+          ]
+        }
+      ],
+      chart: {
+        type: "rangeBar",
+        height: 350
+      },
+      plotOptions: {
+        bar: {
+          horizontal: false
+        }
+      },
+      dataLabels: {
+        enabled: true
+      }
+    };
   }
-
 }
