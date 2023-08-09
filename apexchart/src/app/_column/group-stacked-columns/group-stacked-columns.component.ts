165fea9ccc01190b45db14bfb4a51f37ea06ae69
MotivDesktop1 <shentiexiong@gmial.com>
2023-08-10 11:14:08 +1200

    group-stacked-columns does not support by current version!

--- apexchart/src/app/_column/group-stacked-columns/group-stacked-columns.component.ts
+++ apexchart/src/app/_column/group-stacked-columns/group-stacked-columns.component.ts
@@ -1,15 +1,110 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, ViewChild } from "@angular/core";
+
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexDataLabels,
+  ApexStroke,
+  ApexYAxis,
+  ApexFill,
+  ApexLegend,
+  ApexPlotOptions
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  dataLabels: ApexDataLabels;
+  plotOptions: ApexPlotOptions;
+  stroke: ApexStroke;
+  xaxis: ApexXAxis;
+  yaxis: ApexYAxis;
+  colors: string[];
+  fill: ApexFill;
+  legend: ApexLegend;
+};
 
 @Component({
   selector: 'app-group-stacked-columns',
   templateUrl: './group-stacked-columns.component.html',
   styleUrls: ['./group-stacked-columns.component.css']
 })
-export class GroupStackedColumnsComponent implements OnInit {
+export class GroupStackedColumnsComponent {
+  @ViewChild('chart', { static: false }) chart: any;
+  public chartOptions: ChartOptions;
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor() {
+    this.chartOptions = {
+      series: [
+        {
+          name: "Q1 Budget",
+          // group: "budget",
+          data: [44000, 55000, 41000, 67000, 22000, 43000]
+        },
+        {
+          name: "Q1 Actual",
+          // group: "actual",
+          data: [48000, 50000, 40000, 65000, 25000, 40000]
+        },
+        {
+          name: "Q2 Budget",
+          // group: "budget",
+          data: [13000, 36000, 20000, 8000, 13000, 27000]
+        },
+        {
+          name: "Q2 Actual",
+          // group: "actual",
+          data: [20000, 40000, 25000, 10000, 12000, 28000]
+        }
+      ],
+      chart: {
+        type: "bar",
+        height: 350,
+        stacked: true
+      },
+      stroke: {
+        width: 1,
+        colors: ["#fff"]
+      },
+      dataLabels: {
+        formatter: (val) => {
+          return Number(val) / 1000 + "K";
+        }
+      },
+      plotOptions: {
+        bar: {
+          horizontal: false
+        }
+      },
+      xaxis: {
+        categories: [
+          "Online advertising",
+          "Sales Training",
+          "Print advertising",
+          "Catalogs",
+          "Meetings",
+          "Public relations"
+        ]
+      },
+      fill: {
+        opacity: 1
+      },
+      colors: ["#80c7fd", "#008FFB", "#80f1cb", "#00E396"],
+      yaxis: {
+        labels: {
+          formatter: (val) => {
+            return val / 1000 + "K";
+          }
+        }
+      },
+      legend: {
+        position: "top",
+        horizontalAlign: "left"
+      }
+    };
   }
-
 }
