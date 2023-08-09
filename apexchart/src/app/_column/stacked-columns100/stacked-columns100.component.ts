e16e80e498d728660f1f7a63e6ffffca81a8ec88
MotivDesktop1 <shentiexiong@gmial.com>
2023-08-10 11:09:11 +1200

    app-stacked-columns100

--- apexchart/src/app/_column/stacked-columns100/stacked-columns100.component.ts
+++ apexchart/src/app/_column/stacked-columns100/stacked-columns100.component.ts
@@ -1,15 +1,92 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, ViewChild } from "@angular/core";
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ChartComponent,
+  ApexDataLabels,
+  ApexPlotOptions,
+  ApexResponsive,
+  ApexXAxis,
+  ApexLegend,
+  ApexFill
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  dataLabels?: ApexDataLabels;
+  plotOptions?: ApexPlotOptions;
+  responsive: ApexResponsive[];
+  xaxis: ApexXAxis;
+  legend: ApexLegend;
+  fill: ApexFill;
+};
 
 @Component({
   selector: 'app-stacked-columns100',
   templateUrl: './stacked-columns100.component.html',
   styleUrls: ['./stacked-columns100.component.css']
 })
-export class StackedColumns100Component implements OnInit {
+export class StackedColumns100Component  {
+  @ViewChild('chart', { static: false }) chart: any;
+  public chartOptions: ChartOptions;
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor() {
+    this.chartOptions = {
+      series: [
+        {
+          name: "PRODUCT A",
+          data: [44, 55, 41, 67, 22, 43, 21, 49]
+        },
+        {
+          name: "PRODUCT B",
+          data: [13, 23, 20, 8, 13, 27, 33, 12]
+        },
+        {
+          name: "PRODUCT C",
+          data: [11, 17, 15, 15, 21, 14, 15, 13]
+        }
+      ],
+      chart: {
+        type: "bar",
+        height: 350,
+        stacked: true,
+        stackType: "100%"
+      },
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            legend: {
+              position: "bottom",
+              offsetX: -10,
+              offsetY: 0
+            }
+          }
+        }
+      ],
+      xaxis: {
+        categories: [
+          "2011 Q1",
+          "2011 Q2",
+          "2011 Q3",
+          "2011 Q4",
+          "2012 Q1",
+          "2012 Q2",
+          "2012 Q3",
+          "2012 Q4"
+        ]
+      },
+      fill: {
+        opacity: 1
+      },
+      legend: {
+        position: "right",
+        offsetX: 0,
+        offsetY: 50
+      }
+    };
   }
-
 }
