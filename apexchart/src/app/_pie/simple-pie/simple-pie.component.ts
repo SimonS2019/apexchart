0fc13a9da1801c56de3c2476a9a99bc6156ffb81
MotivDesktop1 <shentiexiong@gmial.com>
2023-11-27 09:29:26 +1300

    light to dark is work

--- apexchart/src/app/_pie/simple-pie/simple-pie.component.ts
+++ apexchart/src/app/_pie/simple-pie/simple-pie.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+import * as ApexCharts from 'apexcharts';
 
 import {
   ChartComponent,
@@ -10,6 +11,7 @@ import {
   ApexFill,
   ApexLegend,
   ApexResponsive,
+  ApexTheme,
 } from 'ng-apexcharts';
 
 export type ChartOptions = {
@@ -22,6 +24,7 @@ export type ChartOptions = {
   plotOptions?: ApexPlotOptions;
   labels: any;
   responsive: ApexResponsive[];
+  theme: ApexTheme;
 };
 @Component({
   selector: 'app-simple-pie',
@@ -29,15 +32,24 @@ export type ChartOptions = {
   styleUrls: ['./simple-pie.component.css'],
 })
 export class SimplePieComponent {
-  @ViewChild('chart', { static: false }) chart: any;
+  @ViewChild('chart', { static: false }) chart: ApexCharts | null = null;
   public chartOptions: ChartOptions;
+  public chartOptions1: ChartOptions;
+  public chartOptions2: ChartOptions;
+  // chart: ApexCharts | null = null;
 
-  constructor() {
-    this.chartOptions = {
+  constructor(private cdr: ChangeDetectorRef) {
+    this.chartOptions1 = {
       series: [44, 55, 13, 43, 22],
       chart: {
         width: 380,
         type: 'pie',
+      },
+      theme: {
+        mode: 'light', // Front end DTO isDarkMode? 'light' : 'dark'
+        palette: 'palette10', //Front-end DTO. basicOptions.selectTheme
+        // we have a dropdown has 11 options 'palette1' to 'palette10' and "Same as ngx-charts", can we have a logic like :
+        //  if (basicOptions.selectTheme = "Same as ngx-charts") we use  this.chartOptions.colors to overwrite this.chartOptions.theme.palette
       },
       labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
       responsive: [
@@ -53,11 +65,105 @@ export class SimplePieComponent {
           },
         },
       ],
+      legend: {
+        show: true, //Front-end DTO. basicOptions.enabledLegend
+        position: 'right', //Front-end DTO. basicOptions.legendPosition
+      },
+    };
+
+    this.chartOptions2 = {
+      series: [44, 55, 13, 43, 22],
+      chart: {
+        width: 380,
+        type: 'pie',
+      },
+      theme: {
+        mode: 'dark', // Front end DTO isDarkMode? 'light' : 'dark'
+        palette: 'palette10', //Front-end DTO. basicOptions.selectTheme
+        // we have a dropdown has 11 options 'palette1' to 'palette10' and "Same as ngx-charts", can we have a logic like :
+        //  if (basicOptions.selectTheme = "Same as ngx-charts") we use  this.chartOptions.colors to overwrite this.chartOptions.theme.palette
+      },
+      labels: ['Team X', 'Team B', 'Team C', 'Team D', 'Team E'],
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200,
+            },
+            legend: {
+              position: 'bottom',
+            },
+          },
+        },
+      ],
+      legend: {
+        show: true, //Front-end DTO. basicOptions.enabledLegend
+        position: 'right', //Front-end DTO. basicOptions.legendPosition
+      },
     };
     setTimeout(() => {
-      console.log(JSON.stringify(this.chartOptions));
-  
+      // console.log(JSON.stringify(this.chartOptions));
     }, 500);
   }
 
+  ngOnInit(): void {
+    console.log('ngOnInit');
+
+    this.chartOptions = { ...this.chartOptions1 };
+    // this.chartOptions.theme.mode = 'light';
+
+    // setTimeout(() => {
+    //   // console.log(JSON.stringify(this.chartOptions));
+    //   this.chartOptions.theme.mode = 'dark';
+    // }, 1000);
+
+    // console.log(JSON.stringify(this.chartOptions));
+  }
+
+  testDarkMode() {
+    console.log('testDarkMode');
+    console.log(this.chartOptions);
+
+    this.chartOptions = {
+      ...this.chartOptions1,
+      theme: {
+        mode: 'dark', // Front end DTO isDarkMode? 'light' : 'dark'
+        palette: 'palette10', //Front-end DTO. basicOptions.selectTheme
+        // we have a dropdown has 11 options 'palette1' to 'palette10' and "Same as ngx-charts", can we have a logic like :
+        //  if (basicOptions.selectTheme = "Same as ngx-charts") we use  this.chartOptions.colors to overwrite this.chartOptions.theme.palette
+      },
+    };
+  }
+  testLightMode() {
+    console.log('testLightMode');
+    // console.log(this.chartOptions);
+
+    this.chartOptions = { ...this.chartOptions1 };
+    // this.cdr.detectChanges();
+    this.chartOptions = {
+      ...this.chartOptions1,
+      theme: {
+        mode: 'light', // Front end DTO isDarkMode? 'light' : 'dark'
+        palette: 'palette10', //Front-end DTO. basicOptions.selectTheme
+        // we have a dropdown has 11 options 'palette1' to 'palette10' and "Same as ngx-charts", can we have a logic like :
+        //  if (basicOptions.selectTheme = "Same as ngx-charts") we use  this.chartOptions.colors to overwrite this.chartOptions.theme.palette
+      },
+    };
+    console.log(this.chartOptions);
+  }
+
+  // testMode() {
+  //   var el = document.getElementById('chart');
+  //   if (el) {
+  //     var chart = new ApexCharts(el, this.chartOptions);
+  //     chart.render();
+  //   }
+  // }
+  // testMode() {
+  //   var el = document.getElementById('chart');
+
+  //   var chart = new ApexCharts(el, this.chartOptions);
+  //   chart.render();
+  // }
 }
