274bf96458437fdbdfc4baf3cc92d88a77a48a92
MotivDesktop1 <shentiexiong@gmial.com>
2024-04-09 09:57:14 +1200

    Update chart IDs and add unixtime chart

--- apexchart/src/app/_line/basic-lin-chart-with-unixtime/basic-lin-chart-with-unixtime.component.ts
+++ apexchart/src/app/_line/basic-lin-chart-with-unixtime/basic-lin-chart-with-unixtime.component.ts
@@ -1,15 +1,82 @@
-import { Component, OnInit } from '@angular/core';
+
+
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexDataLabels,
+  ApexTitleSubtitle,
+  ApexStroke,
+  ApexGrid,
+  ApexTooltip
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  dataLabels: ApexDataLabels;
+  grid: ApexGrid;
+  stroke: ApexStroke;
+  title: ApexTitleSubtitle;
+  tooltip: ApexTooltip;
+
+};
 
 @Component({
   selector: 'app-basic-lin-chart-with-unixtime',
   templateUrl: './basic-lin-chart-with-unixtime.component.html',
   styleUrls: ['./basic-lin-chart-with-unixtime.component.css']
 })
-export class BasicLinChartWithUnixtimeComponent implements OnInit {
+export class BasicLinChartWithUnixtimeComponent {
+  @ViewChild("chart") chart: ChartComponent;
+  public chartOptions: Partial<ChartOptions>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.chartOptions = {
+      series: [
+        {
+          name: "Desktops",
+          data: [
+           [1709550000000, 10],  
+            [1709636399000, 40],
+            [1709722799000, 25],
+          ]
+        }
+      ],
+      chart: {
+        height: 350,
+        type: "line",
+        zoom: {
+          enabled: false
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: "straight"
+      },
+      title: {
+        text: "Product Trends by Month",
+        align: "left"
+      },
+      grid: {
+        row: {
+          colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+          opacity: 0.5
+        }
+      },
+      xaxis: {
+        type: 'datetime'
+      },
+      tooltip: {
+        x: {
+          format: 'hh :mm TT dd MMM yyyy'
+        }
+      },
+    };
   }
-
 }
