2a99756d16ba4d60a05ce085b0b6be42b89cd94d
MotivDesktop1 <shentiexiong@gmial.com>
2024-04-10 09:11:40 +1200

    Update basic-line-chart  use percentage height

--- apexchart/src/app/_line/basic-line-chart/basic-line-chart.component.ts
+++ apexchart/src/app/_line/basic-line-chart/basic-line-chart.component.ts
@@ -7,8 +7,8 @@ import {
   ApexDataLabels,
   ApexTitleSubtitle,
   ApexStroke,
-  ApexGrid
-} from "ng-apexcharts";
+  ApexGrid,
+} from 'ng-apexcharts';
 
 export type ChartOptions = {
   series: ApexAxisChartSeries;
@@ -22,56 +22,68 @@ export type ChartOptions = {
 @Component({
   selector: 'app-basic-line-chart',
   templateUrl: './basic-line-chart.component.html',
-  styleUrls: ['./basic-line-chart.component.css']
+  styleUrls: ['./basic-line-chart.component.css'],
 })
-export class BasicLineChartComponent   {
-  @ViewChild("chart") chart: ChartComponent;
+export class BasicLineChartComponent {
+  @ViewChild('chart') chart: ChartComponent;
   public chartOptions: Partial<ChartOptions>;
+  // show = false;
+  show = true;
 
   constructor() {
     this.chartOptions = {
       series: [
         {
-          name: "Desktops",
-          data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
-        }
+          name: 'Desktops',
+          data: [10, 41, 35, 51, 49, 62, 69, 91, 148],
+        },
       ],
       chart: {
-        height: 350,
-        type: "line",
+        // height: '200px', // why this is not working
+        height: '80%', // why this is not working
+        type: 'line',
         zoom: {
-          enabled: false
-        }
+          enabled: false,
+        },
       },
       dataLabels: {
-        enabled: false
+        enabled: false,
       },
       stroke: {
-        curve: "straight"
+        curve: 'straight',
       },
       title: {
-        text: "Product Trends by Month",
-        align: "left"
+        text: 'Product Trends by Month',
+        align: 'left',
       },
       grid: {
         row: {
-          colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
-          opacity: 0.5
-        }
+          colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+          opacity: 0.5,
+        },
       },
       xaxis: {
         categories: [
-          "Jan",
-          "Feb",
-          "Mar",
-          "Apr",
-          "May",
-          "Jun",
-          "Jul",
-          "Aug",
-          "Sep"
-        ]
-      }
+          'Jan',
+          'Feb',
+          'Mar',
+          'Apr',
+          'May',
+          'Jun',
+          'Jul',
+          'Aug',
+          'Sep',
+        ],
+      },
     };
+
+    // setTimeout(() => {
+    //   console.log(this.chartOptions.chart.height);
+    //   this.chartOptions.chart.height = '200%'; // it is not working
+    //   // this.chartOptions.chart.height = 300; //it is working
+    //   // this.chartOptions.chart.height = '200px'; // it is working
+    //   console.log(this.chartOptions.chart.height);
+    //   this.show = true;
+    // }, 2000);
   }
 }
